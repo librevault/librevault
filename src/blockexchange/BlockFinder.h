4bb2ca8538ce150c90809a2ca995bb60deca044f
Alexander Shishenko <alex@shishenko.com>
2015-06-15 03:44:18 +0300

    Added BlockFinder stub

---
+++ src/blockexchange/BlockFinder.h
@@ -0,0 +1,34 @@
+/* Copyright (C) 2015 Alexander Shishenko <GamePad64@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <boost/property_tree/ptree.hpp>
+
+namespace librevault {
+namespace blockexchange {
+
+using boost::property_tree::ptree;
+
+class BlockFinder {
+	ptree& options;
+	io_service& ios;
+public:
+	BlockFinder(io_service& ios, ptree& options);
+	virtual ~BlockFinder();
+};
+
+} /* namespace blockexchange */
+} /* namespace librevault */

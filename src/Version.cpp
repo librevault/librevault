e81d4b7760a6a7ca312466098585b505a6479dab
Alexander Shishenko <alex@shishenko.com>
2015-10-31 14:56:51 +0300

    Moved "#include "appver.h" to cpp file, because git-commiting forced recompilation every file, that includes Session.h (there are many of these)

--- src/Version.cpp
+++ src/Version.cpp
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "Version.h"
+#include "appver.h"
 
 namespace librevault {
 
--- src/Version.h
+++ src/Version.h
@@ -15,7 +15,6 @@
  */
 #pragma once
 #include "pch.h"
-#include "appver.h"
 
 namespace librevault {
 

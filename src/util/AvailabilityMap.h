50c69d425efa397caad4fecfa67bc0ec3fcc61a4
Alexander Shishenko <alex@shishenko.com>
2015-11-26 04:29:57 +0300

    Added AvailabilityMap utility class (maybe, it will be good to remade cryptodiff with this, instead of that dirty code)

---
+++ src/util/AvailabilityMap.h
@@ -0,0 +1,96 @@
+/* Copyright (C) 2015 Alexander Shishenko <GamePad64@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include <cstdint>
+#include <map>
+
+namespace librevault {
+
+class AvailabilityMap {
+public:
+	struct error : public std::runtime_error {
+		error() : std::runtime_error("AvailabilityMap allocation error") {}
+	};
+
+	using offset_type = uint64_t;
+	using underlying_container = std::map<offset_type, offset_type>;
+	using block_type = std::pair<offset_type, offset_type>;
+	using const_iterator = underlying_container::const_iterator;
+
+	AvailabilityMap(offset_type size) : size_original_(size), size_left_(size) {
+		available_map_.insert({0, size_original_});
+	}
+
+	static bool slice_superset(block_type subset, block_type superset, block_type& block_left, block_type& block_right) {
+		// Initializing with 'invalid' values
+		block_left = block_type(0,0);
+		block_right = block_type(0,0);
+
+		if(subset.second == 0 || superset.second == 0) return false;
+
+		auto subset_last_byte_offset = subset.first+subset.second-1;
+		auto superset_last_byte_offset = superset.first+superset.second-1;
+
+		if(subset.first < superset.first || subset_last_byte_offset > superset_last_byte_offset) return false;
+
+		if(subset.first != superset.first) {
+			block_left.first = superset.first;
+			block_left.second = subset.first - superset.first;
+		}
+
+		if(subset_last_byte_offset != superset_last_byte_offset) {
+			block_right.first = subset_last_byte_offset+1;
+			block_right.second = superset_last_byte_offset - subset_last_byte_offset;
+		}
+		return true;
+	}
+
+	std::pair<const_iterator, bool> insert(block_type block) {
+		if(block.first >= size_original_ || block.first+block.second > size_original_ || available_map_.empty()) return {end(), false};
+
+		auto space_it = available_map_.upper_bound(block.first);	// This returns the block, following the inserted.
+		if(space_it == available_map_.begin()) return {end(), false};
+		--space_it;
+
+		block_type block_left, block_right;
+		if(slice_superset(block, *space_it, block_left, block_right)) {
+			available_map_.erase(space_it);
+			size_left_ -= block.second;
+
+			const_iterator right_it;
+
+			if(block_left != block_type(0,0))
+				available_map_.insert(block_left);
+			if(block_right != block_type(0,0))
+				right_it = available_map_.insert(block_right).first;
+			else
+				right_it = available_map_.upper_bound(block.first);
+
+			return {right_it, true};
+		} else return {end(), false};
+	};
+
+	const_iterator begin() {return available_map_.cbegin();}
+	const_iterator end() {return available_map_.cend();}
+
+	offset_type size_left() const {return size_left_;}
+	offset_type size_original() const {return size_original_;}
+private:
+	offset_type size_original_, size_left_;
+	underlying_container available_map_;
+};
+
+} /* namespace librevault */

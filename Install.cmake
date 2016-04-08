ead9dcb4fbb76f86a3652548bffe18811a7f8a09
Alexander Shishenko <alex@shishenko.com>
2016-04-08 23:34:21 +0300

    Added basic Windows packaging capability

---
+++ Install.cmake
@@ -0,0 +1,26 @@
+if(WIN32)
+	set(LV_PACKAGING_PATH NO CACHE PATH "Base path for packaging purposes")
+	if(NOT LV_PACKAGING_PATH)
+		message(WARNING "Packaging directory is not set")
+	endif()
+
+	set(LV_INTALLER_SOURCE "${LV_PACKAGING_PATH}/release")
+
+	# Daemon
+	if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+		set(LV_DAEMON_PATH "${LV_INTALLER_SOURCE}/x64")
+	else()
+		set(LV_DAEMON_PATH "${LV_INTALLER_SOURCE}/x32")
+	endif()
+
+	install(PROGRAMS $<TARGET_FILE:librevault-daemon> DESTINATION ${LV_DAEMON_PATH} COMPONENT librevault-package)
+
+	# GUI
+	option(BUILD_64BIT_GUI "Build only 64-bit GUI instead of only 32-bit" OFF)
+	if((CMAKE_SIZEOF_VOID_P EQUAL 4 AND NOT BUILD_64BIT_GUI) OR (CMAKE_SIZEOF_VOID_P EQUAL 8 AND BUILD_64BIT_GUI))
+		install(PROGRAMS $<TARGET_FILE:librevault-gui> DESTINATION ${LV_INTALLER_SOURCE} COMPONENT librevault-package)
+	endif()
+elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+	install(PROGRAMS $<TARGET_FILE:librevault-daemon> DESTINATION ${CMAKE_INSTALL_BINDIR} COMPONENT librevault-package)
+	install(PROGRAMS $<TARGET_FILE:librevault-gui> DESTINATION ${CMAKE_INSTALL_BINDIR} COMPONENT librevault-package)
+endif()

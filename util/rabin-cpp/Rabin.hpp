b5bee111f5e9647635bde57a665164a421000867
Alexander Shishenko <alex@shishenko.com>
2017-08-11 21:41:55 +0300

    small changes in rabin-cpp

--- util/rabin-cpp/Rabin.hpp
+++ util/rabin-cpp/Rabin.hpp
@@ -6,30 +6,30 @@
 
 class Rabin {
 public:
-	Rabin(uint64_t polynomial, uint64_t polynomial_shift, uint64_t minsize, uint64_t maxsize, uint64_t mask, uint32_t window_size = 64);
+  Rabin(uint64_t polynomial, uint64_t polynomial_shift, uint64_t minsize, uint64_t maxsize, uint64_t mask, uint32_t window_size = 64);
 
-	bool next_chunk(char b);
-	bool finalize();
+  bool next_chunk(char b);
+  bool finalize();
 
-	uint64_t polynomial;
-	uint64_t polynomial_shift;
-	uint64_t minsize;
-	uint64_t maxsize;
-	uint64_t mask;
+  uint64_t polynomial;
+  uint64_t polynomial_shift;
+  uint64_t minsize;
+  uint64_t maxsize;
+  uint64_t mask;
 
 private:
-	// Lookup tables
-	std::array<uint64_t, 256> mod_table;
-	std::array<uint64_t, 256> out_table;
-	void calc_tables();
+  // Lookup tables
+  std::array<uint64_t, 256> mod_table;
+  std::array<uint64_t, 256> out_table;
+  void calc_tables();
 
-	// Intermediate result
-	boost::circular_buffer<char> window;
+  // Intermediate result
+  boost::circular_buffer<char> window;
 
-	uint64_t count = 0;
-	uint64_t digest = 0;
+  uint64_t count = 0;
+  uint64_t digest = 0;
 
-	void slide(char b);
-	void append(char b);
-	void reset();
+  void slide(char b);
+  void append(char b);
+  void reset();
 };

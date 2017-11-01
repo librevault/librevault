d15fedf2d5c3a9f06c8f1dd806901835c34002aa
Alexander Shishenko <alex@shishenko.com>
2017-11-02 01:23:03 +0300

    use bincrafters spdlog

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 class LibrevaultConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "docopt.cpp/latest@signal9/stable",\
-               "spdlog/latest@signal9/stable",\
+               "spdlog/0.14.0@bincrafters/stable",\
                "sqlite3/3.18.0@jgsogo/stable"
     generators = "cmake"
-    #default_options = ""
+    default_options = ""

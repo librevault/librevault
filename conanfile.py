68dd2140d2d054bd469805dd08cf681b8cc02aba
Alexander Shishenko <alex@shishenko.com>
2017-12-05 02:29:29 +0300

    use cryptopp from bincrafters

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,7 @@ class LibrevaultConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "docopt.cpp/latest@signal9/stable",\
                "spdlog/0.14.0@bincrafters/stable",\
+               "cryptopp/5.6.5@bincrafters/stable",\
                "sqlite3/3.21.0@bincrafters/stable"
     generators = "cmake"
     default_options = ""

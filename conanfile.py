101352012220fa3c983cae30a32cb9b96f3fc5b5
Alexander Shishenko <alex@shishenko.com>
2020-04-15 03:04:35 +0300

    Working on CI

--- conanfile.py
+++ conanfile.py
@@ -13,19 +13,19 @@ class LibrevaultConan(ConanFile):
         "protoc_installer/3.9.1@bincrafters/stable",
         "websocketpp/0.8.1",
         "openssl/1.1.1f",
-        "qt/5.14.2@bincrafters/stable",
+        # "qt/5.14.2@bincrafters/stable",
     ]
     generators = "cmake"
     default_options = {
         "sqlite3:threadsafe": 1,
         "sqlite3:enable_json1": True,
-        "qt:qtsvg": True,
-        "qt:qttranslations": True,
-        "qt:qttools": True,
-        "qt:qtwayland": False,
-        "qt:qtwebsockets": True,
+        # "qt:qtsvg": True,
+        # "qt:qttranslations": True,
+        # "qt:qttools": True,
+        # "qt:qtwayland": False,
+        # "qt:qtwebsockets": True,
     }
 
-    def configure(self):
-        if self.settings.os == "Linux":
-            self.options["qt"].qtwayland = True
+    # def configure(self):
+    #     if self.settings.os == "Linux":
+    #         self.options["qt"].qtwayland = True

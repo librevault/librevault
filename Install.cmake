3d658ad945bc966cf7fbfa8fbb72a77137db6bb6
Alexander Shishenko <alex@shishenko.com>
2020-06-12 17:50:33 +0300

    fix qt build

--- Install.cmake
+++ Install.cmake
@@ -2,12 +2,13 @@ install(CODE "set(CMAKE_INSTALL_LOCAL_ONLY ON)")
 
 include(InstallQt5Plugin)
 
-set(LIBRARY_SEARCH_PATHS "${CMAKE_LIBRARY_PATH};${CONAN_BIN_DIRS};$<$<PLATFORM_ID:Darwin>:${sparkle_SOURCE_DIR}>")
+set(LIBRARY_SEARCH_PATHS "${CMAKE_LIBRARY_PATH};${CONAN_BIN_DIRS};${CONAN_LIB_DIRS};$<$<PLATFORM_ID:Darwin>:${sparkle_SOURCE_DIR}>")
 
 macro(lv_collect_libs INDEPENDENT_BINARIES INSTALL_LOCATION)
 	install(CODE "
 		include(GetPrerequisites)
 		foreach(INSTALLED_BINARY ${INDEPENDENT_BINARIES})
+			string(PREPEND INSTALLED_BINARY \"\$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/\")
 			get_filename_component(INSTALLED_BINARY \${INSTALLED_BINARY} ABSOLUTE)
 			get_prerequisites(\"\${INSTALLED_BINARY}\" dependencies 1 1 \"\" \"${LIBRARY_SEARCH_PATHS}\")
 			foreach(dependency \${dependencies})
@@ -29,11 +30,11 @@ endmacro()
 if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
 	if(BUILD_DAEMON)
 		install(PROGRAMS $<TARGET_FILE:librevault-daemon> DESTINATION ${CMAKE_INSTALL_PREFIX})
-		list(APPEND INSTALLED_BINARIES ${CMAKE_INSTALL_PREFIX}/librevault-daemon.exe)
+		list(APPEND INSTALLED_BINARIES librevault-daemon.exe)
 	endif()
 	if(BUILD_GUI)
 		install(PROGRAMS $<TARGET_FILE:librevault-gui> DESTINATION ${CMAKE_INSTALL_PREFIX})
-		list(APPEND INSTALLED_BINARIES ${CMAKE_INSTALL_PREFIX}/librevault-gui.exe)
+		list(APPEND INSTALLED_BINARIES librevault-gui.exe)
 	endif()
 
 	# Prerequisites
@@ -46,7 +47,6 @@ if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
 	install_qt5_plugin("Qt5::QSvgIconPlugin" QT_PLUGIN "${BUNDLE_PLUGINS_PATH}")
 	install_qt5_plugin("Qt5::QSQLiteDriverPlugin" QT_PLUGIN "${BUNDLE_PLUGINS_PATH}")
 	install_qt5_plugin("Qt5::QWindowsVistaStylePlugin" QT_PLUGIN "${BUNDLE_PLUGINS_PATH}")
-
 	list(APPEND INSTALLED_BINARIES ${QT_PLUGIN})
 
 	lv_collect_libs("${INSTALLED_BINARIES}" "${CMAKE_INSTALL_PREFIX}")
@@ -59,33 +59,28 @@ elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 #	endif()
 	if(BUILD_DAEMON)
 		install(PROGRAMS $<TARGET_FILE:librevault-daemon> DESTINATION ${CMAKE_INSTALL_BINDIR})
-		list(APPEND INSTALLED_BINARIES $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/librevault-daemon)
+		list(APPEND INSTALLED_BINARIES ${CMAKE_INSTALL_BINDIR}/librevault-daemon)
 	endif()
 	if(BUILD_GUI)
 		install(PROGRAMS $<TARGET_FILE:librevault-gui> DESTINATION ${CMAKE_INSTALL_BINDIR})
 		install(FILES "src/gui/resources/Librevault.desktop" DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/applications)
 		install(FILES "src/gui/resources/librevault_icon.svg" DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor/scalable/apps RENAME "librevault.svg")
-		list(APPEND INSTALLED_BINARIES $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/librevault-gui)
+		list(APPEND INSTALLED_BINARIES ${CMAKE_INSTALL_BINDIR}/librevault-gui)
 	endif()
 	if(BUILD_CLI)
 		install(PROGRAMS $<TARGET_FILE:librevault-cli> DESTINATION ${CMAKE_INSTALL_BINDIR})
-		list(APPEND INSTALLED_BINARIES $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/librevault-cli)
+		list(APPEND INSTALLED_BINARIES ${CMAKE_INSTALL_BINDIR}/librevault-cli)
 	endif()
 
 	if(INSTALL_BUNDLE)
 		# Install Qt5 plugins
-		set(BUNDLE_PLUGINS_PATH $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/qt5/plugins)
+		set(BUNDLE_PLUGINS_PATH ${CMAKE_INSTALL_LIBDIR}/qt5/plugins)
 		install_qt5_plugin("Qt5::QMinimalIntegrationPlugin" QT_PLUGIN "${BUNDLE_PLUGINS_PATH}")
 		install_qt5_plugin("Qt5::QXcbIntegrationPlugin" QT_PLUGIN "${BUNDLE_PLUGINS_PATH}")
 		install_qt5_plugin("Qt5::QSvgPlugin" QT_PLUGIN "${BUNDLE_PLUGINS_PATH}")
 		install_qt5_plugin("Qt5::QSvgIconPlugin" QT_PLUGIN "${BUNDLE_PLUGINS_PATH}")
 		install_qt5_plugin("Qt5::QSQLiteDriverPlugin" QT_PLUGIN "${BUNDLE_PLUGINS_PATH}")
-
-		message(STATUS "${LIBRARY_SEARCH_PATHS}")
-
 		list(APPEND INSTALLED_BINARIES ${QT_PLUGIN})
-#		list(APPEND DEPENDENCY_RESOLVE_PATHS "${Qt5_DIR}/../..")
-#		list(APPEND DEPENDENCY_RESOLVE_PATHS "/usr/lib")
 
 		# Dependencies of targets and plugins
 		lv_collect_libs("${INSTALLED_BINARIES}" "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
@@ -137,10 +132,8 @@ elseif(OS_MAC)
 	install(FILES "packaging/osx/dsa_pub.pem" DESTINATION ${APP_ROOT}/Contents/Resources)
 	install(FILES "packaging/osx/Librevault.icns" DESTINATION ${APP_ROOT}/Contents/Resources)
 
-	# Bundle plugin path
-	set(BUNDLE_PLUGINS_PATH "${APP_ROOT}/Contents/PlugIns")
-
 	# Qt5 plugins
+	set(BUNDLE_PLUGINS_PATH "${APP_ROOT}/Contents/PlugIns")
 	install_qt5_plugin("Qt5::QMinimalIntegrationPlugin" QT_PLUGIN "${BUNDLE_PLUGINS_PATH}")
 	install_qt5_plugin("Qt5::QCocoaIntegrationPlugin" QT_PLUGIN "${BUNDLE_PLUGINS_PATH}")
 	install_qt5_plugin("Qt5::QSvgPlugin" QT_PLUGIN "${BUNDLE_PLUGINS_PATH}")

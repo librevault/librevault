d037430df5f18bca9e23d9047671af5b91fa5753
Alexander Shishenko <alex@shishenko.com>
2020-07-01 21:00:04 +0300

    Relicense the project under the terms of MIT license

--- src/common/crypto/Transformer.h
+++ src/common/crypto/Transformer.h
@@ -1,13 +1,3 @@
-/* Written in 2015 by Alexander Shishenko <alex@shishenko.com>
- *
- * LVCrypto - Cryptographic wrapper, used in Librevault.
- * To the extent possible under law, the author(s) have dedicated all copyright
- * and related and neighboring rights to this software to the public domain
- * worldwide. This software is distributed without any warranty.
- *
- * You should have received a copy of the CC0 Public Domain Dedication
- * along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
- */
 #pragma once
 
 #include "util/blob.h"
